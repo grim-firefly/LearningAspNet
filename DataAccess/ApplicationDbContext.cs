6e1a8a764f0bb740954d104b16adae1b277f4d2f
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-04-14 19:55:27 +0600

    added First Commit

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
